2951cd3d3f51345d2d95c6819167eb2523dd6426
ACER <ACER@LAPTOP-MOGB7KD1>
2021-12-03 12:52:28 +0700

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,7 @@
+﻿int bil, ulang, sisa, jumlahbilangan;
+string ket;
+
+Console.Write(“masukan bilangan bulat =”);
+bil = Convert.ToInt16(Console.ReadLine());
+jumlahbilangan = 0;
+for (ulang = 1; ulang <= bil; ulang++) ;
